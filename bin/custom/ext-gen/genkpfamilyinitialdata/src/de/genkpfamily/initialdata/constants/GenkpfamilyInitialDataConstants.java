c1539269bcad72ad92b9b7c64bdf4230d5a1a63e
thesko <franthescollymaneira@Franthescolly.local>
2014-12-02 10:03:33 -0200

    reestruturacao das extensions

---
+++ bin/custom/ext-gen/genkpfamilyinitialdata/src/de/genkpfamily/initialdata/constants/GenkpfamilyInitialDataConstants.java
@@ -0,0 +1,27 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2014 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *
+ *  
+ */
+package de.genkpfamily.initialdata.constants;
+
+/**
+ * Global class for all GenkpfamilyInitialData constants.
+ */
+public final class GenkpfamilyInitialDataConstants extends GeneratedGenkpfamilyInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "genkpfamilyinitialdata";
+
+	private GenkpfamilyInitialDataConstants()
+	{
+		//empty
+	}
+}
