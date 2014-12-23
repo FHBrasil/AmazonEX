1babce90053b44a1470261f96a3da5c15f29f5bb
j.felipe <j.felipe@fliegersoftware.de>
2014-12-23 08:57:08 -0200

    Fixed some issues with Customers and Addresses importing script. KPCountryTypeTranslator javadoc added.

--- bin/custom/ext-kpfamily/kpfamilycore/src/de/kpfamily/core/translators/KPCountryTypeTranslator.java
+++ bin/custom/ext-kpfamily/kpfamilycore/src/de/kpfamily/core/translators/KPCountryTypeTranslator.java
@@ -7,16 +7,41 @@ import de.hybris.platform.jalo.Item;
 import de.hybris.platform.jalo.JaloInvalidParameterException;
 import de.hybris.platform.servicelayer.i18n.CommonI18NService;
 
+/**
+ * Translator used when executing an ImpEx.<br/>
+ * "country(isocode)" property translate to its respective country object on 
+ * target system.
+ * 
+ * @author jfelipe
+ * @version 1.0.0
+ */
 public class KPCountryTypeTranslator extends AbstractValueTranslator {
 
-
+	
+	/**
+	 * Return the respective country as it is.
+	 * 
+	 * @param input country exported
+	 * @return value of input parameter as it is
+	 * 
+	 * @author jfelipe
+	 */
 	@Override
-	public String exportValue(Object arg0)
+	public String exportValue(Object input)
 			throws JaloInvalidParameterException {
-		return String.valueOf(arg0);
+		return String.valueOf(input);
 	}
 
 	
+	/**
+	 * Translates a given input parameter to its equivalent on target system.
+	 * 
+	 * @param input - country from source system to be translated
+	 * @param item - not used
+	 * @return country object equivalent to given input parameter
+	 * 
+	 *  @author jfelipe
+	 */
 	@Override
 	public Object importValue(String input, Item item)
 			throws JaloInvalidParameterException {
