087976ac72b3fa7e08914ab2d52709ab0ccbb3b0
j.felipe <j.felipe@fliegersoftware.de>
2015-02-12 17:02:25 -0200

    Renamed KPProductTaxGroupTranslator.java to KPProductTaxGroupDecorator.java. Refactored impex migration files. New migration file for purchasePriceHistory.

--- bin/custom/ext-kpfamily/kpfamilycore/src/de/kpfamily/core/translators/KPCountryTypeTranslator.java
+++ bin/custom/ext-kpfamily/kpfamilycore/src/de/kpfamily/core/translators/KPCountryTypeTranslator.java
@@ -11,46 +11,45 @@ import de.hybris.platform.servicelayer.i18n.CommonI18NService;
 
 /**
  * Translator used when executing an ImpEx.<br/>
- * "country(isocode)" property translate to its respective country object on 
+ * "country(isocode)" property translate to its respective country object on
  * target system.
  * 
  * @author jfelipe
  */
 public class KPCountryTypeTranslator extends AbstractValueTranslator {
 
-	
-	/**
-	 * Return the respective country as it is, used for exporting data.
-	 * 
-	 * @param input country exported
-	 * @return value of input parameter as it is
-	 * 
-	 * @author jfelipe
-	 */
-	@Override
-	public String exportValue(Object input)
-			throws JaloInvalidParameterException {
-		return String.valueOf(input);
-	}
+    /**
+     * Return the respective country as it is, used for exporting data.
+     * 
+     * @param input
+     *            country exported
+     * @return value of input parameter as it is
+     * 
+     * @author jfelipe
+     */
+    @Override
+    public String exportValue(Object input) throws JaloInvalidParameterException {
+	return String.valueOf(input);
+    }
 
-	
-	/**
-	 * Translates a given input parameter to its equivalent on target system.
-	 * 
-	 * @param input - country from source system to be translated
-	 * @param item - not used
-	 * @return country object equivalent to given input parameter
-	 * 
-	 * @author jfelipe
-	 */
-	@Override
-	public Object importValue(String input, Item item)
-			throws JaloInvalidParameterException {
-		final CommonI18NService commonI18NService = (CommonI18NService)
-				Registry.getApplicationContext().getBean("commonI18NService");
-		input = Strings.isNullOrEmpty(input) ? "de" : input; 
-		CountryModel country = commonI18NService.getCountry(input.toUpperCase());
-		return country.getPk();
-	}
+    /**
+     * Translates a given input parameter to its equivalent on target system.
+     * 
+     * @param input
+     *            - country from source system to be translated
+     * @param item
+     *            - not used
+     * @return country object equivalent to given input parameter
+     * 
+     * @author jfelipe
+     */
+    @Override
+    public Object importValue(String input, Item item) throws JaloInvalidParameterException {
+	final CommonI18NService commonI18NService = (CommonI18NService) Registry
+		.getApplicationContext().getBean("commonI18NService");
+	input = Strings.isNullOrEmpty(input) ? "de" : input;
+	CountryModel country = commonI18NService.getCountry(input.toUpperCase());
+	return country.getPk();
+    }
 
 }
