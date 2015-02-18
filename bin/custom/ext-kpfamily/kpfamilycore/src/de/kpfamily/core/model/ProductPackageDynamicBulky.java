117ab467aa2791b99fe9614a1c9038702fe66283
thesko <franthescollymaneira@Franthescolly.local>
2015-02-18 11:27:54 -0200

    package api

---
+++ bin/custom/ext-kpfamily/kpfamilycore/src/de/kpfamily/core/model/ProductPackageDynamicBulky.java
@@ -0,0 +1,28 @@
+/**
+ *
+ */
+package de.kpfamily.core.model;
+
+import de.hybris.platform.servicelayer.model.attribute.AbstractDynamicAttributeHandler;
+
+
+/**
+ * @author franthescollymaneira
+ *
+ */
+public class ProductPackageDynamicBulky extends AbstractDynamicAttributeHandler<Boolean, ProductPackageModel>
+{
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.hybris.platform.servicelayer.model.attribute.AbstractDynamicAttributeHandler#get(de.hybris.platform.servicelayer
+	 * .model.AbstractItemModel)
+	 */
+	@Override
+	public Boolean get(final ProductPackageModel model)
+	{
+		//TODO implementar aqui
+		return false;
+	}
+}
--- bin/custom/ext-kpfamily/kpfamilyservices/src/de/kpfamily/services/logistics/LogisticsService.java
+++ bin/custom/ext-kpfamily/kpfamilyservices/src/de/kpfamily/services/logistics/LogisticsService.java
@@ -1,9 +1,6 @@
 package de.kpfamily.services.logistics;
 
-import de.kpfamily.core.model.BabyartikelProductModel;
-import de.kpfamily.core.model.PackageSizeModel;
 
 public interface LogisticsService 
 {
-	PackageSizeModel getPackageSizeFromProduct(BabyartikelProductModel product);
 }
