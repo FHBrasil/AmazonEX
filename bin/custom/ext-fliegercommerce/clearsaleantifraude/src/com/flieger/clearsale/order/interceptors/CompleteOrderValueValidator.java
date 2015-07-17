1d1b772a3f94e7d94aad5d16ed920a332b5bcfab
Juliano Felipe <j.felipe@fliegersoftware.de>
2015-07-17 12:55:18 -0300

    Corrige erro capture duplicado na Adyen.

--- bin/custom/ext-fliegercommerce/clearsaleantifraude/src/com/flieger/clearsale/order/interceptors/CompleteOrderValueValidator.java
+++ bin/custom/ext-fliegercommerce/clearsaleantifraude/src/com/flieger/clearsale/order/interceptors/CompleteOrderValueValidator.java
@@ -24,10 +24,10 @@ import com.flieger.constants.AdyenTransactionStatus;
 /**
  * @author flieger
  * @author ezequiel
- * 
  */
 public class CompleteOrderValueValidator implements PrepareInterceptor
 {
+    
     protected final Logger LOG = Logger.getLogger(this.getClass());
     @Resource
     private ModelService modelService;
@@ -37,10 +37,10 @@ public class CompleteOrderValueValidator implements PrepareInterceptor
     private CommonI18NService commonI18NService;
     @Resource
     private AdyenPaymentService paymentService;
-    
     private FlexibleSearchService flexibleSearchService;
     private String paymentProvider;
-
+    
+    
     /**
      * @return the paymentProvider
      */
@@ -48,16 +48,18 @@ public class CompleteOrderValueValidator implements PrepareInterceptor
     {
         return paymentProvider;
     }
-
+    
+    
     /**
      * @param paymentProvider
-     *           the paymentProvider to set
+     *            the paymentProvider to set
      */
     public void setPaymentProvider(final String paymentProvider)
     {
         this.paymentProvider = paymentProvider;
     }
-
+    
+    
     /**
      * @return the customerAccountService
      */
@@ -65,16 +67,18 @@ public class CompleteOrderValueValidator implements PrepareInterceptor
     {
         return customerAccountService;
     }
-
+    
+    
     /**
      * @param customerAccountService
-     *           the customerAccountService to set
+     *            the customerAccountService to set
      */
     public void setCustomerAccountService(final CustomerAccountService customerAccountService)
     {
         this.customerAccountService = customerAccountService;
     }
-
+    
+    
     /**
      * @return the flexibleSearchService
      */
@@ -82,16 +86,18 @@ public class CompleteOrderValueValidator implements PrepareInterceptor
     {
         return flexibleSearchService;
     }
-
+    
+    
     /**
      * @param flexibleSearchService
-     *           the flexibleSearchService to set
+     *            the flexibleSearchService to set
      */
     public void setFlexibleSearchService(final FlexibleSearchService flexibleSearchService)
     {
         this.flexibleSearchService = flexibleSearchService;
     }
-
+    
+    
     /**
      * @return the commonI18NService
      */
@@ -99,16 +105,18 @@ public class CompleteOrderValueValidator implements PrepareInterceptor
     {
         return commonI18NService;
     }
-
+    
+    
     /**
      * @param commonI18NService
-     *           the commonI18NService to set
+     *            the commonI18NService to set
      */
     public void setCommonI18NService(final CommonI18NService commonI18NService)
     {
         this.commonI18NService = commonI18NService;
     }
-
+    
+    
     /**
      * @return the modelService
      */
@@ -116,77 +124,79 @@ public class CompleteOrderValueValidator implements PrepareInterceptor
     {
         return modelService;
     }
-
+    
+    
     /**
      * @param modelService
-     *           the modelService to set
+     *            the modelService to set
      */
     public void setModelService(final ModelService modelService)
     {
         this.modelService = modelService;
     }
-
+    
+    
     /*
      * (non-Javadoc)
-     * 
-     * @see de.hybris.platform.servicelayer.interceptor.ValidateInterceptor#onValidate(java.lang.Object,
+     * @see
+     * de.hybris.platform.servicelayer.interceptor.ValidateInterceptor#onValidate(java.lang.Object
+     * ,
      * de.hybris.platform.servicelayer.interceptor.InterceptorContext)
      */
     @Override
-    public void onPrepare(final Object model, final InterceptorContext ctx) throws InterceptorException
+    public void onPrepare(final Object model, final InterceptorContext ctx)
+            throws InterceptorException
     {
         if (!(model instanceof OrderModel))
         {
             return;
         }
-
         final OrderModel orderModel = (OrderModel) model;
         if (ctx.isModified(orderModel, OrderModel.STATUS))
         {
-            LOG.info("Intercepting order status change. order:{code:" 
-                    + orderModel.getCode() 
-                    + ", status:" 
-                    + (orderModel.getStatus() != null ? orderModel.getStatus().getCode() : "null") 
+            LOG.info("Intercepting order status change. order:{code:"
+                    + orderModel.getCode()
+                    + ", status:"
+                    + (orderModel.getStatus() != null ? orderModel.getStatus().getCode() : "null")
                     + "}");
-            
-            if( OrderStatus.CAPTURE_PAYMENT.equals(orderModel.getStatus())
-                && PaymentStatus.PENDING_APPROVAL.equals(orderModel.getPaymentStatus()))
-        {
-            LOG.info(String.format("Trying to capture. order:{code:%s}",
-                    orderModel.getCode()));
-            
-            final boolean retorno = capturePayment(orderModel);
-            
-            LOG.info(String.format("Order: [%s] was captured? [%s]",
-                    orderModel.getCode(),
-                    String.valueOf(retorno)));
-            
-            if(retorno)
+            if (OrderStatus.CAPTURE_PAYMENT.equals(orderModel.getStatus())
+                    && PaymentStatus.PENDING_APPROVAL.equals(orderModel.getPaymentStatus()))
             {
-                orderModel.setStatus(OrderStatus.PAYMENT_CAPTURED);
-            } else {
-                orderModel.setStatus(OrderStatus.PAYMENT_NOT_CAPTURED);
+                LOG.info(String.format("Trying to capture. order:{code:%s}",
+                        orderModel.getCode()));
+                final boolean retorno = capturePayment(orderModel);
+                LOG.info(String.format("Order: [%s] was captured? [%s]",
+                        orderModel.getCode(),
+                        String.valueOf(retorno)));
+                if (retorno)
+                {
+                    orderModel.setStatus(OrderStatus.PAYMENT_CAPTURED);
+                } else {
+                    orderModel.setStatus(OrderStatus.PAYMENT_NOT_CAPTURED);
+                }
+                getModelService().save(orderModel);
             }
-            getModelService().save(orderModel);
-        } 
         }
     }
-//o adyenservice sempre deve devolver sucesso, se o pedido já foi capturado ou pôde ser capturado nesta tentativa
+    
+    
+    // o adyenservice sempre deve devolver sucesso, se o pedido já foi capturado ou pôde ser
+    // capturado nesta tentativa
     private boolean capturePayment(final OrderModel order)
     {
-        boolean result = true; 
-            
+        boolean result = true;
         for (final PaymentTransactionModel transaction : order.getPaymentTransactions())
         {
-            final PaymentTransactionEntryModel capture = 
-                    paymentService.capture(transaction);
-            
-            //if the capture was received, it means that Adyen received the request for capture, if the capture was accepted, it means that the user account was charged 
-            result &= 
-                    capture != null 
-                    && (AdyenTransactionStatus.CAPTURE_RECEIVED.equals(capture.getTransactionStatus()) 
-                            || TransactionStatus.ACCEPTED.name().equals(capture.getTransactionStatus()));
-        }           
+            final PaymentTransactionEntryModel capture = paymentService.capture(transaction);
+            // if the capture was received, it means that Adyen received the request for capture,
+            // if the capture was accepted, it means that the user account was charged
+            result &=
+                    capture != null
+                            && (AdyenTransactionStatus.CAPTURE_RECEIVED.equals(capture
+                                    .getTransactionStatus())
+                            || TransactionStatus.ACCEPTED.name().equals(
+                                    capture.getTransactionStatus()));
+        }
         return result;
     }
 }
