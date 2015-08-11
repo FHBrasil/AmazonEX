9abb2fb05b8fbf5f5972f8d3a5ac38cb93e5e8d7
Taylor Savegnago <t.savegnago@fliegersoftware.de>
2015-08-11 16:35:30 -0300

    Adicionado reverse populator para SellerOrderAttributes

--- bin/custom/ext-fliegercommerce/amazoncore/src/de/fliegersoftware/amazon/core/job/CancelAmazonOrderJob.java
+++ bin/custom/ext-fliegercommerce/amazoncore/src/de/fliegersoftware/amazon/core/job/CancelAmazonOrderJob.java
@@ -1,25 +1,24 @@
 package de.fliegersoftware.amazon.core.job;
 
+import de.hybris.platform.cronjob.model.CronJobModel;
 import de.hybris.platform.jalo.Item;
 import de.hybris.platform.jalo.JaloBusinessException;
 import de.hybris.platform.jalo.SessionContext;
 import de.hybris.platform.jalo.type.ComposedType;
+import de.hybris.platform.servicelayer.cronjob.AbstractJobPerformable;
+import de.hybris.platform.servicelayer.cronjob.PerformResult;
+
 import org.apache.log4j.Logger;
 
-public class CancelAmazonOrderJob extends GeneratedCancelAmazonOrderJob
+public class CancelAmazonOrderJob extends AbstractJobPerformable<CronJobModel>
 {
-	@SuppressWarnings("unused")
 	private final static Logger LOG = Logger.getLogger( CancelAmazonOrderJob.class.getName() );
-	
+
 	@Override
-	protected Item createItem(final SessionContext ctx, final ComposedType type, final ItemAttributeMap allAttributes) throws JaloBusinessException
-	{
-		// business code placed here will be executed before the item is created
-		// then create the item
-		final Item item = super.createItem( ctx, type, allAttributes );
-		// business code placed here will be executed after the item was created
-		// and return the item
-		return item;
+	public PerformResult perform(CronJobModel cronJob) {
+		// TODO Auto-generated method stub
+		return null;
 	}
+	
 	
 }
