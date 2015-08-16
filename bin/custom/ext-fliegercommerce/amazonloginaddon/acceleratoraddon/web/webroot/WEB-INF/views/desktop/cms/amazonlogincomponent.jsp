bfa20e04546a5b35693a0e3bd244eba3b04246b1
Taylor Savegnago <t.savegnago@fliegersoftware.de>
2015-08-16 11:32:17 -0300

    Corrige amazon login

--- bin/custom/ext-fliegercommerce/amazonloginaddon/acceleratoraddon/web/webroot/WEB-INF/views/desktop/cms/amazonlogincomponent.jsp
+++ bin/custom/ext-fliegercommerce/amazonloginaddon/acceleratoraddon/web/webroot/WEB-INF/views/desktop/cms/amazonlogincomponent.jsp
@@ -1,7 +1,1 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<a href="#" id="LoginWithAmazon">
-      <img border="0" alt="Login with Amazon" src="https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_156x32.png" width="156" height="32" />
-</a>
- 
- <div id="amazon-root">
-</div>
+<div id="LoginWithAmazon"></div>
--- bin/custom/ext-fliegercommerce/amazonloginaddon/acceleratoraddon/web/webroot/_ui/desktop/common/js/amazonloginaddon.js
+++ bin/custom/ext-fliegercommerce/amazonloginaddon/acceleratoraddon/web/webroot/_ui/desktop/common/js/amazonloginaddon.js
@@ -1,42 +1,85 @@
-alert('aaa'); 
+if(!ACC)
+	ACC = {};
+if(!ACC.amazon)
+	ACC.amazon = {};
 
-window.onAmazonLoginReady = function() {
- 	amazon.Login.setClientId('${clientId}');
- };
- (function(d) {
- 	var a = d.createElement('script'); 
- 	a.type = 'text/javascript';
- 	a.async = true; 
- 	a.id = 'amazon-login-sdk';
- 	a.src = 'https://eu.account.amazon.com/lwa/js/sdk/login1.js';
- 	d.getElementById('amazon-root').appendChild(a);
- })(document);
- 
-document.getElementById('Logout').onclick = function() {
-	amazon.Login.logout();
-	console.log("logged out");
-	return false;
-};
+$.getScript(ACC.addons.amazonpaymentaddon.amazonWidgetUrl)
+	.done(function(script, textStatus){
+		window.onAmazonLoginReady = function() {
+			amazon.Login.setClientId(ACC.addons.amazonpaymentaddon.clientId);
+		};
+		if($('#LoginWithAmazon').length) {
+			var authRequest;
+			var returnUrl = ACC.config.contextPath + 'register/login-with-amazon';
+			OffAmazonPayments.Button("LoginWithAmazon", ACC.addons.amazonpaymentaddon.sellerId, {
+				type:  "LwA",
+				color: "Gold",
+				size:  "medium",
+				language: "en-GB",
 
-document.getElementById('LoginWithAmazon').onclick = function() {
-	setTimeout(window.doLogin, 1);
-	return false;
-};
-
-window.doLogin = function(){
-	options = {};
-	options.scope = 'profile';
-	amazon.Login.authorize(options, function(response){
-		if(response.error){
-			alert('oauth error'+response.error);
-			return;
+				authorization: function() {
+					loginOptions =
+						{scope: "profile", popup: "true"};
+					// authRequest = amazon.Login.authorize (loginOptions, returnUrl);
+					amazon.Login.authorize(loginOptions, function(response){
+						if(response.error){
+							alert('oauth error'+response.error);
+							return;
+						}
+						amazon.Login.retrieveProfile(response.access_token, function(response){		
+							window.location=returnUrl+"?name="+response.profile.Name+"&primaryEmail="+response.profile.PrimaryEmail+"&customerId="+response.profile.CustomerId;		
+						});
+					});
+				},
+				onError: function(error) {
+					// your error handling code
+				}
+			});
 		}
-		amazon.Login.retrieveProfile(response.access_token, function(response){		
-			window.location="${respondetUri}?name="+response.profile.Name+"&primaryEmail="+response.profile.PrimaryEmail+"&customerId="+response.profile.CustomerId;		
-			
-			/* alert('Hello '+response.profile.Name);
-			alert('your e-mail address '+response.profile.PrimaryEmail);
-			alert('your unique id '+response.profile.CustomerId); */
-		});
+	})
+	.fail(function( jqxhr, settings, exception ) {
+		//alert( "Triggered ajaxError handler." );
 	});
-};
+
+//window.doLogin = function(){
+//	options = {};
+//	options.scope = 'profile';
+//	amazon.Login.authorize(options, function(response){
+//		if(response.error){
+//			alert('oauth error'+response.error);
+//			return;
+//		}
+//		amazon.Login.retrieveProfile(response.access_token, function(response){		
+//			window.location="${respondetUri}?name="+response.profile.Name+"&primaryEmail="+response.profile.PrimaryEmail+"&customerId="+response.profile.CustomerId;		
+//			
+//			/* alert('Hello '+response.profile.Name);
+//			alert('your e-mail address '+response.profile.PrimaryEmail);
+//			alert('your unique id '+response.profile.CustomerId); */
+//		});
+//	});
+//};
+
+
+//
+//window.onAmazonLoginReady = function() {
+//	amazon.Login.setClientId(''+ACC.addons.amazonpaymentaddon.clientId+'');
+//};
+//(function(d) {
+//	var a = d.createElement('script'); 
+//	a.type = 'text/javascript';
+//	a.async = true; 
+//	a.id = 'amazon-login-sdk';
+//	a.src = 'https://eu.account.amazon.com/lwa/js/sdk/login1.js';
+//	d.getElementById('amazon-root').appendChild(a);
+//})(document);
+
+//document.getElementById('Logout').onclick = function() {
+//	amazon.Login.logout();
+//	console.log("logged out");
+//	return false;
+//};
+
+//document.getElementById('LoginWithAmazon').onclick = function() {
+//	setTimeout(window.doLogin, 1);
+//	return false;
+//};
