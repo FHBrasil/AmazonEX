22719477358d81c4e54186e1e2a6023c263c8e0d
Douglas Canalli <d.canalli@fliegersoftware.de>
2015-08-26 15:58:33 -0300

    hmc changes, adding in the payment info the function to get amazon details

---
+++ bin/custom/ext-fliegercommerce/amazonhmcaddon/hmc/src/de/fliegersoftware/amazon/hmc/action/AmazonAuthorizeAction.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package de.fliegersoftware.amazon.hmc.action;
+
+import de.hybris.platform.hmc.util.action.ActionEvent;
+import de.hybris.platform.hmc.util.action.ActionResult;
+import de.hybris.platform.hmc.util.action.ItemAction;
+import de.hybris.platform.jalo.JaloBusinessException;
+import de.hybris.platform.util.localization.Localization;
+
+import org.apache.log4j.Logger;
+
+
+/**
+ * @author douglas.canalli
+ * 
+ */
+public class AmazonAuthorizeAction extends ItemAction
+{
+	private String errorMessage;
+
+	private static final Logger LOG = Logger.getLogger(AmazonAuthorizeAction.class);
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.hybris.platform.hmc.util.action.ItemAction#perform(de.hybris.platform.hmc.util.action.ActionEvent)
+	 */
+	@Override
+	public ActionResult perform(final ActionEvent paramActionEvent) throws JaloBusinessException
+	{
+		final String successMessage = Localization.getLocalizedString("msg.connection.test.success");
+
+		return new ActionResult(ActionResult.OK, successMessage, false);
+	}
+
+}
